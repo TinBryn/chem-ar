e6d82d2482e65c01791fdde0991b83e9c8f61234
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-10-23 16:07:33 +0800

    Moved matrix math to separate module/file

---
+++ lib/math-3d/src/matrix.rs
@@ -0,0 +1,27 @@
+pub struct Matrix4 {
+    values: [f32; 16],
+}
+
+impl Matrix4 {
+    pub fn identity() -> Self {
+        Self {
+            values: [
+                1.0, 0.0, 0.0, 0.0, //
+                0.0, 1.0, 0.0, 0.0, //
+                0.0, 0.0, 1.0, 0.0, //
+                0.0, 0.0, 0.0, 1.0, //
+            ],
+        }
+    }
+}
+
+/// # Derefs as &[f32]
+///
+/// This is adds all the slice functionality (iteration, etc), in particular
+/// the ability to be passed as a pointer to graphics APIs
+impl std::ops::Deref for Matrix4 {
+    type Target = [f32];
+    fn deref(&self) -> &Self::Target {
+        &self.values
+    }
+}
