862d6c2998f4e16ef61fbcd1bfe26676ff9f3cb9
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-10-24 09:40:53 +0800

    Work on implementing my library on top of gfx_maths

--- lib/math-3d/src/lib.rs
+++ lib/math-3d/src/lib.rs
@@ -3,3 +3,14 @@ pub use vector::*;
 
 pub mod matrix;
 pub use matrix::*;
+
+mod ops_impl {
+    #[macro_export]
+    macro_rules! impl_vec_op {
+        ($trait:tt for $vec:tt of $size:tt with $op:tt) => {
+            impl $trait for $vec{
+                
+            }
+        };
+    }
+}
--- lib/math-3d/src/vector.rs
+++ lib/math-3d/src/vector.rs
@@ -1,58 +1,63 @@
-#[derive(Debug, Default, Clone, Copy, PartialEq, PartialOrd)]
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
+#[repr(C)]
 pub struct Vector3 {
-    values: [f32; 3],
+    rep: gfx_maths::Vec3,
+}
+
+pub struct Vector2 {
+    rep: gfx_maths::Vec2,
+}
+
+impl Vector2 {
+    pub fn new(rep: gfx_maths::Vec2) -> Self {
+        Self { rep }
+    }
 }
 
 impl Vector3 {
     pub fn new(x: f32, y: f32, z: f32) -> Self {
-        Self { values: [x, y, z] }
+        Self {
+            rep: gfx_maths::Vec3 { x, y, z },
+        }
     }
 
     pub fn dot(self, rhs: Vector3) -> f32 {
-        self.values.into_iter().zip(rhs).map(|(l, r)| l * r).sum()
+        self.rep.dot(rhs.rep)
+    }
+
+    pub fn cross(self, rhs: Vector3) -> Vector3 {
+        Self {
+            rep: self.rep.cross(rhs.rep),
+        }
     }
 }
 
-/// # Consuming IntoIterator
-///
-/// This would allow an `Iterator<Item = Vector3>` to be flattened into `Iterator<Item = f32>`
-impl IntoIterator for Vector3 {
-    type Item = f32;
-    type IntoIter = std::array::IntoIter<f32, 3>;
-    fn into_iter(self) -> Self::IntoIter {
-        self.values.into_iter()
-    }
-}
-
-pub trait ArrayWrapper<T>: Sized {
-    fn wrapped_slice_as_base_slice(slice: &[Self]) -> &[T];
-}
-
-impl ArrayWrapper<f32> for Vector3 {
-    fn wrapped_slice_as_base_slice(slice: &[Self]) -> &[f32] {
-        let ptr = slice.as_ptr() as *const f32;
-        let len = slice.len();
-
-        unsafe { std::slice::from_raw_parts(ptr, len * 3) }
-    }
-}
-
-/// # Derefs as &[f32]
-///
-/// This is adds all the slice functionality (iteration, etc), in particular
-/// the ability to be passed as a pointer to graphics APIs
 impl std::ops::Deref for Vector3 {
     type Target = [f32];
     fn deref(&self) -> &Self::Target {
-        &self.values
+        // # Safety not sure but this implementation is temporary as I move away from this lib
+        unsafe { std::slice::from_raw_parts((&self.rep.x) as *const f32, 3) }
     }
 }
 
-/// # Derefs as &mut [f32]
-///
-/// As with the Deref, but allows mutation
 impl std::ops::DerefMut for Vector3 {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.values
+        // # Safety not sure but this implementation is temporary as I move away from this lib
+        unsafe { std::slice::from_raw_parts_mut((&mut self.rep.x) as *mut f32, 3) }
+    }
+}
+
+impl std::ops::Deref for Vector2 {
+    type Target = [f32];
+    fn deref(&self) -> &Self::Target {
+        // # Safety not sure but this implementation is temporary as I move away from this lib
+        unsafe { std::slice::from_raw_parts((&self.rep.x) as *const f32, 2) }
+    }
+}
+
+impl std::ops::DerefMut for Vector2 {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        // # Safety not sure but this implementation is temporary as I move away from this lib
+        unsafe { std::slice::from_raw_parts_mut((&mut self.rep.x) as *mut f32, 2) }
     }
 }
